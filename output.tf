9c0eb13e14308fe693344d36e4c0e267f503f417
Tim Jacomb <21194782+timja@users.noreply.github.com>
2022-12-19 16:44:24 +0000

    Add != string "false"

--- output.tf
+++ output.tf
@@ -2,6 +2,6 @@ output "common_tags" {
   description = "Returns a mapping of tags to assign."
   value = {
     for k, v in local.all_tags : k => v
-    if v != false && v != ""
+    if v != false && v != "" && v != "false"
   }
 }
