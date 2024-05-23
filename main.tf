ddf445631a05138ce6e65518bcfaf0fc4a5d46a3
Chirag Pareek <111435448+cpareek@users.noreply.github.com>
2024-05-23 13:44:32 +0100

    DTSPO-17731 - change expiresAfter default days (#71)

--- main.tf
+++ main.tf
@@ -13,10 +13,10 @@ locals {
     autoShutdown = var.autoShutdown
     startupMode  = var.startupMode
   }
+  # if var.expiresAfter set to "0000-00-00", set default date to date of creation + 14 days
+  expiresAfter = var.expiresAfter == "0000-00-00" ? formatdate("YYYY-MM-DD", timeadd(timestamp(), "336h")) : var.expiresAfter
+  all_tags     = local.common_tags.environment == "sandbox" ? merge(local.common_tags, local.additional_tags, tomap({ "expiresAfter" = local.expiresAfter })) : merge(local.common_tags, local.additional_tags)
 
-  expiresAfter = var.expiresAfter == "0000-00-00" ? formatdate("YYYY-MM-DD", timeadd(timestamp(), "720h")) : var.expiresAfter
-  all_tags = local.common_tags.environment == "sandbox" ? merge(local.common_tags, local.additional_tags, tomap({"expiresAfter" = local.expiresAfter})) : merge(local.common_tags, local.additional_tags)
-  
   criticality = {
     sbox     = "Low"
     aat      = "High"
